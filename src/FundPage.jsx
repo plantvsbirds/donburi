b7059c710c8e066d64f006f98602ac63026dc7d0
Aluba Mister <plantvsbird@gmail.com>
2020-02-14 00:56:40 -0800

    fund page tx item component

--- src/FundPage.jsx
+++ src/FundPage.jsx
@@ -2,32 +2,41 @@ import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 
 import vis from 'vis-network'
+import db from './db.js'
 
 const Tx = ({data}) => {
+  const Item = ({title, dat}) => {
+    return (
+      <div>
+        <p className="text-m text-left text-gray-600 font-bold uppercase">
+            state name
+        </p>
+        <h1 className="text-xl">
+            {dat}
+        </h1>
+      </div>
+    )
+  }
   return (
-    <div className="break-all">
+    <div className="break-all bg-gray-100 rounded p-4 m-5">
+        <div className="text-xl text-center m-1 text-blue-600">
+            Transaction Details
+        </div>
+        {data && data.name && <Item title="state name" dat={data.name}/>}
       {JSON.stringify(data)}
     </div>
   )
 }
 
-const Edges = [
-  { from: 'Proposal', to: 'Pre-sale' },
-  { from: 'Pre-sale', to: 'Approval' },
-  { from: 'Pre-sale', to: 'Closed' },
-  { from: 'Approval', to: 'Sale' },
-  { from: 'Sale', to: 'Closed' }
-]
-const States = [ 'Proposal', 'Pre-sale', 'Closed', 'Approval', 'Sale' ]
 const nodeMaker = (sName) => {
   return {
     id: sName,
     label: sName,
   }
 }
-var nodes = new vis.DataSet(States.map(nodeMaker));
+var nodes = new vis.DataSet(db.States.map(nodeMaker));
 // create an array with edges
-var edges = new vis.DataSet(Edges);
+var edges = new vis.DataSet(db.Edges);
 
 const stateLookupFromSelectEvt = (x) => x.nodes.length > 0 ? nodes.get(x.nodes[0]) : edges.get(x.edges[0])
 
