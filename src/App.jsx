4426bf6b37c8ff3746499122981bc5d1587e9e73
Aluba Mister <plantvsbird@gmail.com>
2020-02-14 00:56:19 -0800

    improve nav code

--- src/App.jsx
+++ src/App.jsx
@@ -3,67 +3,71 @@ import { useEffect, useRef, useState } from 'react'
 
 
 import FundPage from './FundPage'
+import PropertiesPage from './Properties'
+import Transactions from './Transactions'
+
 
 const App = () => {
+  const NavRole = ({txt, selected}) => {
+    return (
+        selected ? (
+          <li className="-mb-px mr-1">
+          <div className="capitalize bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">
+            {txt}
+          </div>
+          </li>
+        ) : (
+          <li className="mr-1">
+            <div className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
+              {txt}
+            </div>
+          </li>
+        )
+    )
+  }
+  const NavPage = ({txt, selected}) => {
+    return (
+        selected ? (
+          <li className="-mb-px mr-1">
+            <div className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">
+              
+            {txt}
+            
+            </div>
+          </li>
+        ) : (
+          <li className="mr-1">
+            <div className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
+              
+            {txt}
+            
+            </div>
+          </li>
+        )
+    )
+  }
   return (
     <div>
       <ul className="bg-gray-200 flex main-nav border-b user-nav">
         <li className="-mb-px mr-1">
-          <a className="inline-block py-2 px-4 text-black-700 font-light" href="#">
-            
+          <p className="inline-block py-2 px-4 text-black-700 font-light" href="#">
           Logged in as
-          
-          </a>
+          </p>
         </li>
-        <li className="-mb-px mr-1">
-          <a className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">
-            
-          Fund manager
-          
-          </a>
-        </li>
-        <li className="mr-1">
-          <a className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
-            
-          Property owner
-          
-          </a>
-        </li>
-        <li className="mr-1">
-          <a className="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
-            
-          Shareholder
-          
-          </a>
-        </li>
+        <NavRole txt="fund manager" selected />
+        <NavRole txt="Property owner" />
+        <NavRole txt="Shareholder" />
+        
       </ul>
       <ul className="flex main-nav border-b">
         <li className="mr-1">
-          <a className="bg-white inline-block py-2 px-4 text-purple-700 font-semibold" href="">
+          <div className="bg-white inline-block py-2 px-4 text-purple-700 font-semibold" href="">
             Donburi | A Posei demo
-          </a>
+          </div>
         </li>
-        <li className="-mb-px mr-1">
-          <a className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="#">
-            
-          Fund status
-          
-          </a>
-        </li>
-        <li className="mr-1">
-          <a className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
-            
-          Properties
-          
-          </a>
-        </li>
-        <li className="mr-1">
-          <a className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="#">
-            
-          Transactions
-          
-          </a>
-        </li>
+        <NavPage txt="fund status" selected />
+        <NavPage txt="Properties" />
+        <NavPage txt="Transactions" />
       </ul>
       <div className="mx-auto h-screen">
         <FundPage />
